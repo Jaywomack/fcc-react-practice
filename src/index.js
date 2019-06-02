a2996e72610c4b4d8b7a557e4d5be2cd28928748
Jared Womack <womackjw@hotmail.com>
2019-06-01 17:02:24 -0700

    Finished the Functional Components Practice

--- src/index.js
+++ src/index.js
@@ -10,3 +10,61 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// function MyApp() { //functional component that returns the JSX below. Which is rendered below by the ReactDOM.render() method
+//     return (
+//     <ul>
+//        <li>asdf</li> 
+//        <li>sadf</li>
+//        <li>asdf</li>
+//     </ul>
+//         ) //The li's above would not work without the ul because only one jsx element can be rendered to the dom at a time
+    
+// }
+
+// ReactDOM.render(
+//     <MyApp />, //Self closing tag
+//     document.getElementById('root') //selects the node that will be rendered to.
+// )
+
+// ReactDOM.render(
+//     <ul>
+//       <li>asdf</li>
+//       <li>sadf</li>
+//       <li>asdf</li>
+//     </ul>,
+//     document.getElementById('root')
+//   )
+
+//   ReactDOM.render(
+//       <div>
+//           <ul>
+//               <li>1</li>
+//               <li>2</li>
+//               <li>3</li>
+//               <li>4</li>
+//               <li>5</li>
+//           </ul>
+//       </div>,
+//       document.getElementById('root')
+//   )
+
+
+function MyInfo() {
+   return( <div>
+       <h1>Jared Womack</h1>
+        <p>I am supreme overlord of the universe</p>
+        <ul>
+            <li>Lisbon, Portugal</li>
+            <li>Barcelona, Spain</li>
+            <li>Nice, France</li>
+        </ul>
+    </div>
+   )
+}
+
+ReactDOM.render(
+    <MyInfo />,
+    document.getElementById('root')
+)
