22e233ab36dddcdf02f91a0f2721130e348eea5d
Jared Womack <womackjw@hotmail.com>
2019-06-02 09:59:54 -0700

    Created a page layout and styled with css and used string literals withing jsx

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,23 @@ import MainContent from './MainContent';
 import Footer from './Footer'
 
 function App() {
+  const firstName ='Jared'
+  const lastName ='Womack'
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if( hours < 12) {
+    timeOfDay = 'morning'
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = 'afternoon'
+  } else{
+    timeOfDay = 'night'
+  }
+
   return (
     <div>
+      <h1>Hello, {`${firstName} ${lastName}`} it is currently about {date.getHours() % 12} o'clock! Good {`${timeOfDay}`} </h1>
       <Navbar />
       <MainContent />
       <Checkbox />
