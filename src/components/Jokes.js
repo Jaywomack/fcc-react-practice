f81e1acbccba77caf9ebef6f40778e2fb9a894ec
Jared Womack <womackjw@hotmail.com>
2019-06-02 14:34:17 -0700

    added inline styles with jokes

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Jokes(props) {
     return (
         <div className='joke-card'>
-            <h3>{props.joke.name}</h3>
-            <p>{props.joke.question}</p>
-            <p>{props.joke.punchline}</p>
+            <h3 style={{backgroundColor: 'black', color: 'white'}}>{props.joke.name}</h3>
+            <p style={{backgroundColor: 'red', color: 'white'}}>{props.joke.question}</p>
+            <p style={{backgroundColor: 'green', color: 'yellow'}}>{props.joke.punchline}</p>
         </div>
     )
 }
